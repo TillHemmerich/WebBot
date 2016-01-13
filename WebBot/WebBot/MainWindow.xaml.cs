e5fb404eff54bb30db7eb7bbd3cf162492770bd2
TillHemmerich <till.hemmerich@hotmail.de>
2016-01-13 20:05:11 +0100

    Browser Fixx
    Fixxes Browser

--- WebBot/WebBot/MainWindow.xaml.cs
+++ WebBot/WebBot/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +26,8 @@ namespace WebBot
         public MainWindow()
         {
             InitializeComponent();
+            var appName = Process.GetCurrentProcess().ProcessName + ".exe";
+            SetIE8KeyforWebBrowserControl(appName);
             design des = new design();
             animationEnterStart();
             browser.Navigate("http://de.ikariam.gameforge.com/");
@@ -67,6 +71,66 @@ namespace WebBot
             myDoubleAnimationInWindow2.From = 800;
 
             sb2.Begin();
+        }
+
+        //Browser
+        private void SetIE8KeyforWebBrowserControl(string appName)
+        {
+            RegistryKey Regkey = null;
+            try
+            {
+
+                //For 64 bit Machine 
+                if (Environment.Is64BitOperatingSystem)
+                    Regkey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\\Wow6432Node\\Microsoft\\Internet Explorer\\MAIN\\FeatureControl\\FEATURE_BROWSER_EMULATION", true);
+                else  //For 32 bit Machine 
+                    Regkey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION", true);
+
+                //If the path is not correct or 
+                //If user't have priviledges to access registry 
+                if (Regkey == null)
+                {
+                    MessageBox.Show("Application Settings Failed - Address Not found");
+                    return;
+                }
+
+                string FindAppkey = Convert.ToString(Regkey.GetValue(appName));
+
+                //Check if key is already present 
+                if (FindAppkey == "8000")
+                {
+                    MessageBox.Show("Required Application Settings Present");
+                    Regkey.Close();
+                    return;
+                }
+
+                //If key is not present add the key , Kev value 8000-Decimal 
+                if (string.IsNullOrEmpty(FindAppkey))
+                    Regkey.SetValue(appName, unchecked((int)0x1F40), RegistryValueKind.DWord);
+
+                //check for the key after adding 
+                FindAppkey = Convert.ToString(Regkey.GetValue(appName));
+
+                if (FindAppkey == "8000")
+                    MessageBox.Show("Application Settings Applied Successfully");
+                else
+                    MessageBox.Show("Application Settings Failed, Ref: " + FindAppkey);
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Application Settings Failed");
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //Close the Registry 
+                if (Regkey != null)
+                    Regkey.Close();
+            }
+
+
         }
     }
 }
