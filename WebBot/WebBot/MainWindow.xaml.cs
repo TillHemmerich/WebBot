f771cf3f89266c3a7a2d315cbb77617e7b869e54
Syr0kx <sven-spieler@web.de>
2016-01-13 20:19:51 +0100

    Revert "sdf"
    This reverts commit 781cf2f42ac2787544f50d120bf3e81e348dacc8.

--- WebBot/WebBot/MainWindow.xaml.cs
+++ WebBot/WebBot/MainWindow.xaml.cs
@@ -29,13 +29,48 @@ namespace WebBot
             var appName = Process.GetCurrentProcess().ProcessName + ".exe";
             SetIE8KeyforWebBrowserControl(appName);
             design des = new design();
-            des.animationEnterStart();
+            animationEnterStart();
             browser.Navigate("http://de.ikariam.gameforge.com/");
         }
 
         private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
+        }
+
+        //design
+        public void animationEnterStart()
+        {
+            Duration duration05 = new Duration(TimeSpan.FromSeconds(1.5));
+
+            DoubleAnimation myDoubleAnimationInWindow1 = new DoubleAnimation();
+            DoubleAnimation myDoubleAnimationInWindow2 = new DoubleAnimation();
+
+            myDoubleAnimationInWindow1.Duration = duration05;
+            myDoubleAnimationInWindow2.Duration = duration05;
+
+            Storyboard sb1 = new Storyboard();
+            Storyboard sb2 = new Storyboard();
+
+            sb1.Duration = duration05;
+            sb2.Duration = duration05;
+
+            sb1.Children.Add(myDoubleAnimationInWindow1);
+            sb2.Children.Add(myDoubleAnimationInWindow1);
+
+            Storyboard.SetTarget(myDoubleAnimationInWindow1, mainwindow);
+            Storyboard.SetTarget(myDoubleAnimationInWindow2, mainwindow);
+
+            Storyboard.SetTargetProperty(myDoubleAnimationInWindow1, new PropertyPath("(Height)"));
+            Storyboard.SetTargetProperty(myDoubleAnimationInWindow2, new PropertyPath("(Width)"));
+
+
+            myDoubleAnimationInWindow1.From = 0;
+            myDoubleAnimationInWindow2.From = 0;
+            myDoubleAnimationInWindow1.To = 600;
+            myDoubleAnimationInWindow2.From = 800;
+
+            sb2.Begin();
         }
 
         //Browser
--- WebBot/WebBot/design.cs
+++ WebBot/WebBot/design.cs
@@ -7,39 +7,5 @@ public class design
 	public design()
 	{
 	}
-    WebBot.MainWindow mainwindow = new WebBot.MainWindow();
-    public void animationEnterStart()
-    {
-        Duration duration05 = new Duration(TimeSpan.FromSeconds(1.5));
-        Duration duration04 = new Duration(TimeSpan.FromSeconds(0.5));
 
-        DoubleAnimation myDoubleAnimationInWindow1 = new DoubleAnimation();
-        DoubleAnimation myDoubleAnimationInWindow2 = new DoubleAnimation();
-
-        myDoubleAnimationInWindow1.Duration = duration05;
-        myDoubleAnimationInWindow2.Duration = duration04;
-
-        Storyboard sb1 = new Storyboard();
-        Storyboard sb2 = new Storyboard();
-
-        sb1.Duration = duration05;
-        sb2.Duration = duration04;
-
-        sb1.Children.Add(myDoubleAnimationInWindow1);
-        sb2.Children.Add(myDoubleAnimationInWindow1);
-
-        Storyboard.SetTarget(myDoubleAnimationInWindow1, mainwindow);
-        Storyboard.SetTarget(myDoubleAnimationInWindow2, mainwindow);
-
-        Storyboard.SetTargetProperty(myDoubleAnimationInWindow1, new PropertyPath("(Height)"));
-        Storyboard.SetTargetProperty(myDoubleAnimationInWindow2, new PropertyPath("(Width)"));
-
-
-        myDoubleAnimationInWindow1.From = 0;
-        myDoubleAnimationInWindow2.From = 0;
-        myDoubleAnimationInWindow1.To = 600;
-        myDoubleAnimationInWindow2.From = 800;
-
-        sb2.Begin();
-    }
 }
